7120dca357ed659670bdb56a0436d98382b9be3a
Mateo Upegui Borja <upeguiborja@gmail.com>
2024-04-29 07:46:00 -0500

    Commit inicial

---
+++ src/main/java/dev/x4d/holidaysapi/HolidaysApiApplication.java
@@ -0,0 +1,13 @@
+package dev.x4d.holidaysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaysApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/x4d/holidaysapi/HolidaysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.x4d.holidaysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
